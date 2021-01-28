16894bd778f5d244487a85570015a303952cf49b
EduardEsteba <38983280+EduEsteba@users.noreply.github.com>
2021-01-28 12:25:47 +0100

    creem usuari i bd pels clients

--- fitxers_apache.php
+++ fitxers_apache.php
@@ -5,27 +5,55 @@ include "db.php";
 $nom_site=$_POST['site'];
 
 
-
 $sql = $connection->prepare("SELECT * FROM sites where nom_site = :nomsite");
 $sql->bindParam("nomsite",$nom_site,PDO::PARAM_STR);
 $sql->execute();
 
 if ($sql->rowCount()>0){
     header('Location: formulari_nou_site.html');
-
 }
 
 else{
+    /*CREEM DIRECTORI SITE*/
+    echo $nom_site;
+    $ruta=('/etc/var/www/' . $nom_site);
+    echo $ruta;
+    mkdir($ruta, 0777, true);
 
+    /*Registrem a la taula el site creat*/
     $insert = $connection->prepare("INSERT INTO sites (nom_site,usuari) values (:nomsite,'usuari2')");
     $insert->bindParam("nomsite",$nom_site,PDO::PARAM_STR);
     $insert->execute();
 
-    echo $nom_site;
-    $ruta=('/etc/var/www/' . $nom_site);
+    /*Creem una base de dades pel site del client*/
+    $servername = "localhost";
+    $username = "root";
+    $password = "1234";
 
-    echo $ruta;
-    mkdir($ruta, 0777, true);
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $createdatabase = "CREATE DATABASE $nom_site";
+    $conn->exec($createdatabase);
+
+
+    /*$base_client = $connection->prepare("CREATE DATABASE :nombd");
+    $base_client -> bindParam("nombd", $nomsite,PDO::PARAM_STR);
+    $base_client -> execute();*/
+
+    $phpmyadmin = $connection->prepare("CREATE USER jaume1@'localhost' IDENTIFIED BY '1234'");
+    $phpmyadmin->execute();
+
+    $permisos = $connection->prepare("GRANT ALL PRIVILEGES ON :dbclient TO jaume1@localhost");
+    $permisos -> bindParam(":dbclient", $nomsite,PDO::PARAM_STR);
+    $permisos->execute();
+
+
+
+
+
+
+
+   
 
     header('Location: panell.html');
 }
