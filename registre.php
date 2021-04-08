63e32e482d776d8f8aa0337b91dc81b1ff6fdd9f
EduardEsteba <38983280+EduEsteba@users.noreply.github.com>
2021-04-08 12:17:05 +0200

    registre
    registre fet

---
+++ registre.php
@@ -0,0 +1,105 @@
+<?php
+require_once "config.php";
+ 
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+ 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    if(empty(trim($_POST["username"]))){
+    } else{
+        $sql = "SELECT id FROM users WHERE username = ?";
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            $param_username = trim($_POST["username"]);
+            
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } 
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+    if(empty(trim($_POST["password"]))){
+    }
+    else{
+        $password = trim($_POST["password"]);
+    }
+    
+    if(empty(trim($_POST["confirm_password"]))){
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+        }
+    }
+    
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+        
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+            
+            if(mysqli_stmt_execute($stmt)){
+                header("location: login.php");
+            } 
+
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Registre</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css.css">
+</head>
+<body class="registre_body">
+    <div class="container text-center">
+    <div class="row justify-content-md-center">
+    <div class="col-4 formulari center-h center-v">
+        
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <h2>Registre</h2>
+        <p>Omple els següent formulari</p>
+            <div class="form-group">
+                <label>Nom d'usuari</label>
+                <input type="text" name="username" placeholder="Nom usuari" class="form-control<?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Contrasenya</label>
+                <input type="password" name="password" placeholder="Introdueix la contrasenya" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Torna a esciure la contrasenya</label>
+                <input type="password" name="confirm_password" placeholder="Confirma la contrasenya" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Enviar">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Ja tens compte? <a href="login.php">Inicia sessió aqui</a>.</p>
+        </form>
+    </div>
+    </div>  
+    </div>  
+</body>
+</html>
